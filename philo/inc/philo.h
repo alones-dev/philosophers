42f48a0ff2c7843d45b06c0857cfb70148a4434f
Kylian Daumont <kdaumont@bess-f3r3s8.clusters.42paris.fr>
2024-01-19 13:42:47 +0100

    init project

---
+++ philo/inc/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/19 13:27:35 by kdaumont          #+#    #+#             */
+/*   Updated: 2024/01/19 13:32:44 by kdaumont         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
